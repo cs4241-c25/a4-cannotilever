cd52029029ed275fb424c8cc9cc5ec821c318006
cannotilever <cannotilever.xyz>
2025-02-18 21:00:39 -0500

    Add edit functionality

--- src/app/api/pantry/[id]/add/route.js
+++ src/app/api/pantry/[id]/add/route.js
@@ -13,11 +13,15 @@ export async function POST(req, {params}) {
         const collection = db.collection("items");
         const data = await req.json();
         console.log("Got POST request", data);
-        const { itemname, category, purchase_date } = data;
+        const { id, itemname, category, purchase_date } = data;
         if (!itemname || !category || !purchase_date) {
             return NextResponse.json({ success: false, message: 'Missing required fields' }, { status: 400 });
         }
-        console.log('Received data:', { itemname, category, purchase_date });
+        if (id){
+            console.log('Updating entry:', { id });
+            return await updateRecord(id, itemname, category, purchase_date);
+        }
+        console.log('Received data:', { id, itemname, category, purchase_date });
         const result = await collection.insertOne({ user_id: new ObjectId(user_id), name: itemname, category: category, purchase_date: purchase_date, safe: true });
         // Respond back with a success message
         if (result.insertedId) {
@@ -29,4 +33,16 @@ export async function POST(req, {params}) {
 
     // Handle other HTTP methods if necessary
     return NextResponse.error();
+}
+
+async function updateRecord(id, itemname, category, purchase_date) {
+    const client = await clientPromise;
+    const db = client.db("Pantry");
+    const collection = db.collection("items");
+    const result = await collection.updateOne({ _id: new ObjectId(id) }, { $set: { name: itemname, category: category, purchase_date: purchase_date } });
+    if (result.modifiedCount) {
+        return NextResponse.json({ success: true, message: 'Data updated successfully' });
+    } else {
+        return NextResponse.json({ success: false, message: 'Failed to update data' }, { status: 500 });
+    }
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 'use client'
+import React, { useState } from 'react';
 import Box from '@mui/material/Box';
-import * as React from 'react';
 import Button from '@mui/material/Button';
 import EditIcon from '@mui/icons-material/Edit';
 import CssBaseline from '@mui/material/CssBaseline';
@@ -23,32 +23,33 @@ import AddIcon from '@mui/icons-material/Add';
 import Typography from '@mui/material/Typography';
 import theme from './theme';
 import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined';
-import {DataGrid} from '@mui/x-data-grid';
+import { DataGrid } from '@mui/x-data-grid';
 import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
 import useSWR from 'swr';
-import {ThemeProvider} from "@mui/material/styles";
+import { mutate } from 'swr';
 
-const fetcher = url => fetch(url).then(r => r.json())
+import { ThemeProvider } from "@mui/material/styles";
+
+const fetcher = url => fetch(url).then(r => r.json());
 
 const userId = '65d1f9c6e1d3a3b4c2d9a001'; // TODO: TEMPORARY
 
-
-function inventoryTable() {
-    const { data, error, isLoading } = useSWR(`/api/pantry/${userId}/view`, fetcher)
+function InventoryTable({ onEdit }) {
+    const { data, error, isLoading } = useSWR(`/api/pantry/${userId}/view`, fetcher);
     if (error) return (
         <Box justifyContent={'center'} alignItems={'center'}>
             <ErrorOutlineIcon />
         </Box>
-    )
+    );
     if (isLoading) return (
         <Box justifyContent={'center'} alignItems={'center'}>
             <CircularProgress />
         </Box>
-    )
+    );
     const paginationModel = { page: 0, pageSize: 15 };
     const columns = [
         { field: 'name', headerName: 'Item Name', minWidth: 120, flex: 1 },
-        { field: 'category', headerName: 'Category', minWidth: 100, flex: 1},
+        { field: 'category', headerName: 'Category', minWidth: 100, flex: 1 },
         {
             field: 'purchase_date',
             headerName: 'Date Purchased',
@@ -60,7 +61,7 @@ function inventoryTable() {
         {
             field: 'isSafe',
             headerName: 'Safe to use',
-            description: 'This column presents weather the item is safe to use or not based on category and purchase date',
+            description: 'This column presents whether the item is safe to use or not based on category and purchase date',
             minWidth: 70,
             flex: 1,
             type: 'boolean',
@@ -74,7 +75,7 @@ function inventoryTable() {
             renderCell: (params) => (
                 <Grid2 container spacing={2} alignItems={'center'} justifyContent={'center'}>
                     <Grid2 size='auto'>
-                        <button onClick={() => handleEdit(params.row)}>
+                        <button onClick={() => onEdit(params.row)}>
                             <EditIcon />
                         </button>
                     </Grid2>
@@ -99,19 +100,15 @@ function inventoryTable() {
             rows={rows}
             columns={columns}
             density={'comfortable'}
-            // autosizeOnMount={true}
             initialState={{ pagination: { paginationModel } }}
             pageSizeOptions={[10, 15, 20]}
             disableRowSelectionOnClick
         />
-    )
+    );
 }
 
-function handleEdit(row) {
-    console.log("Edit row:", row)
-}
 async function handleDelete(row) {
-    console.log("Delete row:", row)
+    console.log("Delete row:", row);
     const response = fetch(`/api/pantry/${userId}/delete`, {
         method: 'POST',
         headers: {
@@ -119,127 +116,150 @@ async function handleDelete(row) {
         },
         body: JSON.stringify({ id: row.id })
     });
+    if (response.ok) {
+        console.log("Item deleted successfully");
+       await mutate(`/api/pantry/${userId}/view`); // force refresh of table
+    }
 }
 
-
 export default function Home() {
-    const [open, setOpen] = React.useState(false);
+    const [open, setOpen] = useState(false);
+    const [formData, setFormData] = useState({});
+
     const handleClickOpen = () => {
+        setFormData({});
         setOpen(true);
     };
 
     const handleClose = () => {
         setOpen(false);
     };
-  return (
-      <ThemeProvider theme={theme}>
-        <CssBaseline enableColorScheme/>
-          <Box sx={{ flexGrow: 1 }}>
-              <AppBar position="static">
-                  <Toolbar>
-                      <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                          Kitchen Inventory Manager
-                      </Typography>
-                      <Button color="inherit">Login</Button>
-                  </Toolbar>
-              </AppBar>
-          </Box>
 
-          <Box justifyContent={'center'} alignItems={'center'} sx={{ display: 'flex', flexDirection: 'column', marginTop: 10 }}>
-              <Paper elevation={2} sx={{ width: '85%' }}>
-                  {inventoryTable()}
-              </Paper>
-          </Box>
+    const handleEdit = (row) => {
+        setFormData({
+            id: row.id,
+            itemname: row.name,
+            category: row.category,
+            purchase_date: row.purchase_date,
+        });
+        setOpen(true);
+    };
+
+    return (
+        <ThemeProvider theme={theme}>
+            <CssBaseline enableColorScheme />
+            <Box sx={{ flexGrow: 1 }}>
+                <AppBar position="static">
+                    <Toolbar>
+                        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                            Kitchen Inventory Manager
+                        </Typography>
+                        <Button color="inherit">Login</Button>
+                    </Toolbar>
+                </AppBar>
+            </Box>
+
+            <Box justifyContent={'center'} alignItems={'center'} sx={{ display: 'flex', flexDirection: 'column', marginTop: 10 }}>
+                <Paper elevation={2} sx={{ width: '85%' }}>
+                    <InventoryTable onEdit={handleEdit} />
+                </Paper>
+            </Box>
             <Box justifyContent={'center'} alignItems={'center'} sx={{ display: 'flex', flexDirection: 'column', marginTop: 10 }}>
                 <Fab size="large" color="primary" aria-label="add" sx={{ position: 'fixed', bottom: 10, right: 10 }} onClick={handleClickOpen}>
                     <AddIcon />
                 </Fab>
             </Box>
-          <Dialog
-              open={open}
-              onClose={handleClose}
-              slotProps={{
-                  paper: {
-                      component: 'form',
-                      onSubmit: (event) => {
-                          event.preventDefault();
-                          const formData = new FormData(event.currentTarget);
-                          const formJson = Object.fromEntries(formData.entries());
-                          console.log(formJson);
-                          // POST to api endpoint
-                          const response = fetch(`/api/pantry/${userId}/add`, {
-                              method: 'POST',
-                              headers: {
-                                  'Content-Type': 'application/json'
-                              },
-                              body: JSON.stringify(formJson)
-                          });
-                          if (response.ok) {
-                              console.log("Item added successfully");
-                          }
-                          handleClose();
-                      },
-                  },
-              }}
-          >
-              <DialogTitle>Add New Item</DialogTitle>
-              <DialogContent>
-                  <DialogContentText>
-                      Input the information for your new pantry item.
-                  </DialogContentText>
-                  <Box marginBottom={2}>
-                  <TextField
-                      autoFocus
-                      required
-                      margin="cozy"
-                      id="name"
-                      name="itemname"
-                      label="Item Name"
-                      type="text"
-                      fullWidth
-                      variant="standard"
-                  />
-                  </Box>
-                  <Box marginBottom={2}>
-                  <TextField
-                      required
-                      margin="cozy"
-                        id="category"
-                        name="category"
-                        label="Category"
-                        select
-                        fullWidth
-                      >
-                      <MenuItem value="Fruit">Fruit</MenuItem>
-                      <MenuItem value="Vegetable">Vegetable</MenuItem>
-                      <MenuItem value="Meat">Meat</MenuItem>
-                      <MenuItem value="Dairy">Dairy</MenuItem>
-                      <MenuItem value="Grain">Grain</MenuItem>
-                      <MenuItem value="Other">Other</MenuItem>
-                  </TextField>
-                  </Box>
-                  <Box marginBottom={2}>
-                    <TextField
-                        required
-                        margin="cozy"
-                        id="purchase_date"
-                        name="purchase_date"
-                        label="Purchase Date"
-                        type="date"
-                        fullWidth
-                        variant="standard"
-                        slotProps={{ inputLabel: { shrink: true } }}
-                    />
-                  </Box>
+            <Dialog
+                open={open}
+                onClose={handleClose}
+                slotProps={{
+                    paper: {
+                        component: 'form',
+                        onSubmit: (event) => {
+                            event.preventDefault();
+                            const formData = new FormData(event.currentTarget);
+                            const formJson = Object.fromEntries(formData.entries());
+                            console.log(formJson);
+                            // POST to api endpoint
+                            const response = fetch(`/api/pantry/${userId}/add`, {
+                                method: 'POST',
+                                headers: {
+                                    'Content-Type': 'application/json'
+                                },
+                                body: JSON.stringify(formJson)
+                            });
+                            if (response.ok) {
+                                console.log("Item added successfully");
+                                mutate(`/api/pantry/${userId}/view`); // force refresh of table
+                            }
+                            else {
+                                console.log("Failed to add item: ", response);
+                            }
 
-              </DialogContent>
-              <DialogActions>
-                  <Button onClick={handleClose}>Cancel</Button>
-                  <Button type="submit">Submit</Button>
-              </DialogActions>
-          </Dialog>
-
-      </ThemeProvider>
-
-  );
+                            handleClose();
+                        },
+                    },
+                }}
+            >
+                <DialogTitle>{formData.itemname ? "Edit Item" : "Add New Item"}</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        Input the information for your pantry item.
+                    </DialogContentText>
+                    <input type="hidden" id="id" name="id" value={formData.id || ''} />
+                    <Box marginBottom={2}>
+                        <TextField
+                            autoFocus
+                            required
+                            margin="cozy"
+                            id="name"
+                            name="itemname"
+                            label="Item Name"
+                            type="text"
+                            fullWidth
+                            variant="standard"
+                            defaultValue={formData.itemname || ''}
+                        />
+                    </Box>
+                    <Box marginBottom={2}>
+                        <TextField
+                            required
+                            margin="cozy"
+                            id="category"
+                            name="category"
+                            label="Category"
+                            select
+                            fullWidth
+                            defaultValue={formData.category || ''}
+                        >
+                            <MenuItem value="Fruit">Fruit</MenuItem>
+                            <MenuItem value="Vegetable">Vegetable</MenuItem>
+                            <MenuItem value="Meat">Meat</MenuItem>
+                            <MenuItem value="Dairy">Dairy</MenuItem>
+                            <MenuItem value="Grain">Grain</MenuItem>
+                            <MenuItem value="Other">Other</MenuItem>
+                        </TextField>
+                    </Box>
+                    <Box marginBottom={2}>
+                        <TextField
+                            required
+                            margin="cozy"
+                            id="purchase_date"
+                            name="purchase_date"
+                            label="Purchase Date"
+                            type="date"
+                            fullWidth
+                            variant="standard"
+                            defaultValue={formData.purchase_date || ''}
+                            slotProps={{ inputLabel: { shrink: true } }}
+                        />
+                    </Box>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose}>Cancel</Button>
+                    <Button type="submit">Submit</Button>
+                </DialogActions>
+            </Dialog>
+        </ThemeProvider>
+    );
 }
